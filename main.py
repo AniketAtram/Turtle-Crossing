a3f750f39702aebc5227184a0c2adf73de325911
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-08-24 14:03:41 +0530

    Script to run the game

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ player = Player()
 car_manager = CarManager()
 score_board = Scoreboard()
 
-
+# TODO: ENABLE KEYBOARD INPUT TO MOVE TURTLE FORWARDS
 screen.listen()
 screen.onkey(player.move_forward, "Up")
 game_is_on = True
@@ -24,12 +24,14 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
+# TODO: CHECK IF THE TURTLE HAS COLLIDED WITH CARS, IF TRUE THEN DISPLAY THE GAME OVER! MESSAGE AND END GAME
     # detect collision with cars
     for car in car_manager.cars_list:
         if car.distance(player) < 20:
             game_is_on = False
             score_board.game_over()
 
+# TODO: CHECK IF TURTLE HAS REACH THE END, IF IT HAS, INCREASE THE LEVEL AND RESET THE TURTLE POSITION
     # detect if the turtle has reached the end of the line
     if player.ycor() == 280:
         score_board.update_score()
